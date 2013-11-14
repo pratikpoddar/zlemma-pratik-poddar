d570a5f5893a59b51bd3ad3254e4834031f1db5e
Pratik Poddar <pratik.phodu@gmail.com>
2013-11-15 00:26:44 +0530

    Fixing E[x][x]=1.0 before propagation for so-large

--- floyd-warshall.py
+++ floyd-warshall.py
@@ -20,8 +20,10 @@ def floydwarshall(matrix, skills):
 				outputmatrix[skillx][skilly]=matrix[skillx][skilly]
 			except:
 				outputmatrix[skillx][skilly] = SMALL_NUM
-	
-	
+
+	for skill in skills:
+		outputmatrix[skill][skill] = 1.0
+
 	for skill1 in skills:
 		for skill2 in skills:
 			for skill3 in skills:
