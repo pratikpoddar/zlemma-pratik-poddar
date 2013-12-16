223f56f8ef89e8d09735026777a90b2786324c39
Pratik Poddar <pratik.phodu@gmail.com>
2013-12-16 09:28:12 +0530

    Changes in Manual Input

--- saveAllResults.sh
+++ saveAllResults.sh
@@ -4,8 +4,8 @@ cd ../mathse-large
 python skill_graph_so.py saveresults
 cd ../so-quant-large
 python skill_graph_so.py saveresults
-cd ../wikilang
-python wikipedia_lang.py saveresults
-cd ../wiki_tfidf
-python download_skills.py saveresults
+#cd ../wikilang
+#python wikipedia_lang.py saveresults
+#cd ../wiki_tfidf
+#python download_skills.py saveresults
 
