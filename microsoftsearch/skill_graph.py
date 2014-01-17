d76c26f267b53c1b9030c26f2b789376541577bf
Pratik Poddar <pratik.phodu@gmail.com>
2014-01-17 13:40:25 +0530

    Microsoft Academic Search Working

--- microsoftsearch/skill_graph.py
+++ microsoftsearch/skill_graph.py
@@ -11,7 +11,7 @@ import gevent
 from gevent import monkey
 from gevent.pool import Pool
 import json
-from tor_urllib2 import getUrl
+#from tor_urllib2 import getUrl
 
 monkey.patch_all(thread=False)
 
@@ -26,6 +26,7 @@ def getMSdata(skill):
 	f = open('htmldump/' + skill, 'r')
 	data = f.read()
 	f.close()
+	print "Found skill " + skill + " from file"
     except:
 	print "Could not get " + skill + " data"
     return data
@@ -37,14 +38,13 @@ def getorcreateMSdata(kwid):
 	except:
 		try:
 			print kwid
-			##opener = urllib2.build_opener(urllib2.HTTPCookieProcessor(), urllib2.ProxyHandler({}))
-                	##opener.addheaders = [('User-Agent', 'Mozilla/5.0 (Windows NT 5.1; rv:10.0.1) Gecko/20100101 Firefox/10.0.1'), ('Accept', 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8')]
+			opener = urllib2.build_opener(urllib2.HTTPCookieProcessor(), urllib2.ProxyHandler({'http':'127.0.0.1:8118'}))
+                	opener.addheaders = [('User-Agent', 'Mozilla/5.0 (Windows NT 5.1; rv:10.0.1) Gecko/20100101 Firefox/10.0.1'), ('Accept', 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8')]
 			url = "http://academic.research.microsoft.com/Keyword/"+str(kwid)
 			print url
-                	##filedata = opener.open(url)
-			print url
-			##data = filedata.read()
-			data = getUrl(url)
+                	filedata = opener.open(url)
+			data = filedata.read()
+			###### data = getUrl(url)
 			print str(kwid) + "------" + data[0:100]
 			parsed = BeautifulSoup(data)
 			if parsed.title.text:
@@ -69,9 +69,10 @@ def getKeywordData(skill):
 
 if sys.argv[1] == "downloaddata":
 	#getorcreateMSdata(int(sys.argv[2]))
-	for i in range(1,100):
-		getorcreateMSdata(i)
-	#pool = Pool(100)
-	#jobs = [pool.spawn(getorcreateMSdata , n) for n in range(0,100)]
-	#pool.join()
+	#for i in range(1,100):
+	#	getorcreateMSdata(i)
+	for i in range(2, 100):
+		pool = Pool(200)
+		jobs = [pool.spawn(getorcreateMSdata , n) for n in range(i*200,(i+1)*200)]
+		pool.join()
 
--- microsoftsearch/tor_urllib2.py
+++ microsoftsearch/tor_urllib2.py
@@ -1,6 +1,6 @@
 import StringIO
 import socket
-import urllib
+import urllib2
 
 import socks  # SocksiPy module
 import stem.process
@@ -9,20 +9,43 @@ from stem.util import term
 
 SOCKS_PORT = 7000
 
+# Set socks proxy and wrap the urllib module
+
+socks.setdefaultproxy(socks.PROXY_TYPE_SOCKS5, '127.0.0.1', SOCKS_PORT)
+socket.socket = socks.socksocket
+
+# Perform DNS resolution through the socket
+
+def getaddrinfo(*args):
+    return [(socket.AF_INET, socket.SOCK_STREAM, 6, '', (args[0], args[1]))]
+
+socket.getaddrinfo = getaddrinfo
+
+# Set socks proxy and wrap the urllib module
+
+socks.setdefaultproxy(socks.PROXY_TYPE_SOCKS5, '127.0.0.1', SOCKS_PORT)
+socket.socket = socks.socksocket
+
+def getaddrinfo(*args):
+    return [(socket.AF_INET, socket.SOCK_STREAM, 6, '', (args[0], args[1]))]
+
+socket.getaddrinfo = getaddrinfo
+
+def print_bootstrap_lines(line):
+     if "Bootstrapped " in line:
+       print term.format(line, term.Color.BLUE)
+
+print term.format("Starting Tor:\n", term.Attr.BOLD)
+
+tor_process = stem.process.launch_tor_with_config(
+     config = {
+        'SocksPort': str(SOCKS_PORT),
+        'ExitNodes': '{ru}',
+     },
+     init_msg_handler = print_bootstrap_lines,
+)
+
 def getUrl(url):
-
-	# Set socks proxy and wrap the urllib module
-
-	socks.setdefaultproxy(socks.PROXY_TYPE_SOCKS5, '127.0.0.1', SOCKS_PORT)
-	socket.socket = socks.socksocket
-
-	# Perform DNS resolution through the socket
-
-	def getaddrinfo(*args):
-	  return [(socket.AF_INET, socket.SOCK_STREAM, 6, '', (args[0], args[1]))]
-	
-	socket.getaddrinfo = getaddrinfo
-
 
 	def query(url):
 	  """
@@ -30,32 +53,19 @@ def getUrl(url):
 	  """
 
 	  try:
-	    return urllib.urlopen(url).read()
-	  except:
-	    return "Unable to reach %s" % url
-
-
-	# Start an instance of tor configured to only exit through Russia. This prints
-	# tor's bootstrap information as it starts. Note that this likely will not
-	# work if you have another tor instance running.
-
-	def print_bootstrap_lines(line):
-	  if "Bootstrapped " in line:
-	    print term.format(line, term.Color.BLUE)
-
-
-	print term.format("Starting Tor:\n", term.Attr.BOLD)
-
-	tor_process = stem.process.launch_tor_with_config(
-	  config = {
-	    'SocksPort': str(SOCKS_PORT),
-	    'ExitNodes': '{ru}',
-	  },
-	  init_msg_handler = print_bootstrap_lines,
-	)
-
+	      opener = urllib2.build_opener(urllib2.HTTPCookieProcessor())
+  	      hdr = {'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.64 Safari/537.11',
+                     'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+                     'Accept-Charset': 'ISO-8859-1,utf-8;q=0.7,*;q=0.3',
+                     'Accept-Encoding': 'none',
+                     'Accept-Language': 'en-US,en;q=0.8',
+                     'Connection': 'keep-alive'}
+              opener.addheaders = hdr.items()
+	      return opener.open(url).read()
+	  except Exception as e:
+	    return "Unable to reach " + url + " " + str(e)
 
 	output = query(url)
-	tor_process.kill()  # stops tor
+	#tor_process.kill()  # stops tor
 	return output
 
