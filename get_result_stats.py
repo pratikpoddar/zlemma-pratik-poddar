c056ff3f7edc0abdaa6e394fc4dc432c41278dc6
Pratik Poddar <pratik.phodu@gmail.com>
2014-01-21 17:49:34 +0530

    Minor

--- get_result_stats.py
+++ get_result_stats.py
@@ -6,7 +6,6 @@ import networkx as nx
 import operator
 
 bins = [0.0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0]
-print(bins)
 
 def slugify(title):
     name = title.replace(' ', '-').lower()
