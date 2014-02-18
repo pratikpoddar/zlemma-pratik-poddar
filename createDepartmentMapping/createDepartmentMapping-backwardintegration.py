c6d558afc48fd42ecc9a28f5c8761ea846f8cc56
Pratik Poddar <pratik.phodu@gmail.com>
2014-02-18 18:38:20 +0530

    Create Department Mapping

---
+++ createDepartmentMapping/createDepartmentMapping-backwardintegration.py
@@ -0,0 +1,47 @@
+import json
+import urllib2
+
+def getSkillsfromTagGraph(skillfile):
+        with open(skillfile, 'r') as infile:
+            data=json.load(infile)
+        return data.keys()
+
+def getSkillsfromDepartmentMapping(skillfile):
+        with open(skillfile, 'r') as infile:
+            data=json.load(infile)
+        return data.keys()
+
+
+def getSkillsFromDepartmentMappingLink(googledocid, outputfile='manualdepartmentmapping.txt'):
+
+	print 'https://spreadsheets.google.com/feeds/list/'+googledocid+'/od6/public/basic?hl=en_US&alt=json'
+	d = json.load(urllib2.urlopen('https://spreadsheets.google.com/feeds/list/'+googledocid+'/od6/public/basic?hl=en_US&alt=json'))
+	output = {}
+
+	for entry in d['feed']['entry']:
+        	try:
+                	output[entry['title']['$t']] = entry['content']['$t']
+	        except:
+        	        pass
+
+	with open(outputfile, 'w') as infile:
+                infile.write(json.dumps(output))
+	return output.keys()
+
+def createDepMapping(skillfiletocheck, googledocidtocheck):
+	l1 = getSkillsfromTagGraph(skillfiletocheck)
+	l2 = getSkillsFromDepartmentMappingLink(googledocidtocheck, outputfile=skillfiletocheck+'-manualdepartmentmapping.txt')
+	print "Skills to be removed from Mapping File"
+	for i in  sorted(list(set(l2)-set(l1))):
+		print i
+	print "Skills to be added to Mapping File"
+	for i in sorted(list(set(l1)-set(l2))):
+		print i
+	return
+
+createDepMapping('V1.0_tag_graph_data.txt', '0Ang7Vqp7FIPSdEFMN0UxbU1EWkdrbG1nbThnRU5jd3c')
+createDepMapping('V2.0_tag_graph_data.txt', '0Ang7Vqp7FIPSdFJnR2hKeXBXRmowX0hveDk3cWxYZXc')
+createDepMapping('V3.0_tag_graph_data.txt', '0AjPfOfZ0QaixdGEzZ2djdHJRVFgtR3pZejhVQlZnSEE')
+
+
+
